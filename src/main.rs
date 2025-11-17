d565d6005fd0c3fcb44616977951cbc887c5dadd
cankocak <can_kocak1@epam.com>
2025-11-17 10:37:03 +0300

    modified the return type to handle empty arrays 😎

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,16 @@
-fn find_min(arr: &[i32]) -> i32 {
-    let mut min = arr[0];
-
-    for i in arr {
-        if *i < min {
-            min = *i;
-        }
-    }
-    return min;
+fn find_min(arr: &[i32]) -> Option<i32> {
+    arr.iter().min().copied()
 }
 
-fn find_max(arr: &[i32]) -> i32 {
-    let mut max = arr[0];
-
-    for i in arr {
-        if *i > max {
-            max = *i;
-        }
-    }
-    return max;
+fn find_max(arr: &[i32]) -> Option<i32> {
+    arr.iter().max().copied()
 }
 
 fn main() {
     let arr = [1, 3, 5, -1, 10, -2, 4, 7, 19, -13];
-    let min = find_min(&arr);
-    let max = find_max(&arr);
-    println!("min: {0} | max: {1}", min, max);
+
+    match (find_min(&arr), find_max(&arr)) {
+        (Some(min), Some(max)) => println!("min: {0} | max: {1}", min, max),
+        _ => println!("Array is empty."),
+    }
 }
